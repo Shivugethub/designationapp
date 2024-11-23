b3f2cebdcfe5d8bba02577dc7d408ff36df6ac82
Shivu K <33868554+Shivugethub@users.noreply.github.com>
2024-11-23 13:24:17 +0530

    feat: filter, edit, toast message
    feat: filter, edit, toast message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,26 @@ import { NgModule } from '@angular/core';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AppComponent } from './app.component';
 import { DesignationdisplayComponent } from './designationdisplay/designationdisplay.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule, NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { from } from 'rxjs';
 import { DataService } from './data.service';
+import { ToastsContainerComponent } from './toasts-container/toasts-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DesignationdisplayComponent,
+    ToastsContainerComponent,
     ],
   imports: [
     BrowserModule,
     FormsModule,
     NgbModule,
     HttpClientModule,
-    InMemoryWebApiModule.forRoot(DataService)
+    InMemoryWebApiModule.forRoot(DataService),
+    NgbToastModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/designationdisplay/designationdisplay.component.ts
+++ src/app/designationdisplay/designationdisplay.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Designation } from 'src/app/designation';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { DesignationdataService } from '../designationdata.service';
+import { ToastService } from '../toast.service';
 // import { jquery } from 'jquery-confirm';
 // import { JsonpClientBackend } from '@angular/common/http';
 
@@ -20,10 +21,11 @@ export class DesignationdisplayComponent implements OnInit {
   description: string = '';
 
   //  using api
-  constructor(private modalService: NgbModal, private dataservice: DesignationdataService) {}
+  constructor(private modalService: NgbModal, private dataservice: DesignationdataService, private toastService: ToastService) {}
   errorMessage = '' ;
   editDesignation: Designation[];
-  arrdesignation: Designation[] = [] ;
+  arrdesignation: Designation[] = [];
+  storeDesignation: Designation[] = [];
   // End api
 
   ngOnInit() {
@@ -34,6 +36,7 @@ export class DesignationdisplayComponent implements OnInit {
     this.dataservice.getDesignations().subscribe((data: any[]) => {
       console.log(data);
       this.arrdesignation = data;
+      this.storeDesignation = [...data];
     });
   }
 
@@ -43,13 +46,7 @@ export class DesignationdisplayComponent implements OnInit {
       this.dataservice.deleteDesignation(arrKey.Designation).subscribe((ret: any) => {
         console.log('Successfuly deleted: ', ret);
         let deletedData = this.arrdesignation.splice(this.arrdesignation.indexOf(arrKey), 1);
-        alert('Deleted Designaton:' + "'" + deletedData[0].Designation + "'" + '  Description:' + "'" + deletedData[0].Description + "'");
-
-        // jquery-confirm
-        //   $.alert({
-        //     title: 'Alert!',
-        //     content: 'Simple alert!',
-        // });
+        this.toastService.show(`Deleted: ${deletedData[0].Designation}`, { classname: 'bg-danger text-light', delay: 150 });
       });
     }
   }
@@ -90,5 +87,31 @@ export class DesignationdisplayComponent implements OnInit {
       );
     }
     this.modalService.dismissAll();
+  }
+
+  onSearch(searchTerm: HTMLButtonElement) {
+    this.arrdesignation = searchTerm.value ? this.storeDesignation.filter((desgn : Designation) => desgn.Designation.toLowerCase().includes(searchTerm.value.toLowerCase())) : 
+     this.storeDesignation;
+  }
+
+
+  addNewDesg() {
+    const id = Math.random().toString();
+    const newDesgn = {Id: id, Designation:this.designation, Description: this.description };
+    this.arrdesignation.push(newDesgn);
+    this.storeDesignation.push(newDesgn);
+    console.table(this.arrdesignation);
+    this.toastService.show('Designation Added');
+  }
+
+  updateDesg() {
+    const desgnObj = this.arrdesignation.find(item => item.Id == this.id.toString());
+    const itemIndex = this.arrdesignation.findIndex(item => item.Id == this.id.toString());
+    this.toastService.show('Designation Updated', { classname: 'bg-success text-light', delay: 100 });
+    this.arrdesignation[itemIndex] = { Id: this.id.toString(), Designation: this.designation, Description: this.description};
+  }
+
+  closeDialog() {
+    
   }
 }
